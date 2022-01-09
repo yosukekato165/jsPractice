454a1bd594cdb9617972102b5372f47c7daedc33
yosukekato165 <padyk165@gmail.com>
2022-01-09 14:39:31 +0900

    touch NullishCoalescingOperator.js

---
+++ BinaryLogicalOperators/NullishCoalescingOperator.js
@@ -0,0 +1,17 @@
+// ?? NullishCoalescingOperator
+// ?? null または undefined でなければ Truthy な値を返す
+
+const ZERO = 0;
+const ONE = 1;
+
+// console 1
+console.log(null ?? ONE);
+
+// console 1
+console.log(undefined ?? ONE);
+
+// console 0
+console.log(ZERO ?? ONE);
+
+// console 0
+console.log(ZERO ?? null);
