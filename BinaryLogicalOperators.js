723198603b57fb9cbc6357b431c0e601e65ae6a9
yosukekato165 <padyk165@gmail.com>
2022-01-09 14:12:44 +0900

    mv ??.js to BinaryLogicalOperators.js and add example of Logical And

---
+++ BinaryLogicalOperators.js
@@ -0,0 +1,11 @@
+// && Logical And
+// && かつ
+const ZERO = 0;
+const ONE = 1;
+const TWO = 2;
+
+// truthy
+console.log(ONE > ZERO && TWO > ZERO);
+
+// falsy
+console.log(ZERO > ZERO && ONE > ZERO);
